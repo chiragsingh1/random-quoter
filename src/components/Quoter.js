732d4da0bc6e0a43c6c245edaa8cda4e1baa2310
chiragsingh1 <imchirag.2717@gmail.com>
2021-08-22 20:09:09 +0530

    fetch quote from the api and display

--- src/components/Quoter.js
+++ src/components/Quoter.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Quoter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quote: "",
+    };
+  }
+
+  fetchQuote = () => {
+    axios
+      .get("	https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({
+          quote: advice,
+        });
+      })
+      .catch((error) => {
+        if (error) {
+          console.log("ERROR ! :(  ", error);
+        }
+      });
+  };
+
+  componentDidMount() {
+    this.fetchQuote();
+  }
+
   render() {
     return (
       <div className="quote-container">
         <div className="quote">
-          <p>QUOTE!!!</p>
+          <p>{this.state.quote}</p>
         </div>
         <div className="btn-container">
-          <button>CLICK ME!</button>
+          <button onClick={this.fetchQuote}>Fetch a new Quote!</button>
         </div>
       </div>
     );
